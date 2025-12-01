1b89c7b16f6623fac61b04c5d10b0a132853f46c
Keith Dunkin <keithdunkin@Keiths-MacBook-Air.local>
2025-12-01 15:26:22 -0600

    Fix [slug] page default export

--- app/modules/[slug]/page.tsx
+++ app/modules/[slug]/page.tsx
@@ -1,6 +1,9 @@
+// app/modules/[slug]/page.tsx
+
 import ModulePageClient from "./ModulePageClient";
 
 export default function Page({ params }: { params: { slug: string } }) {
   return <ModulePageClient slug={params.slug} />;
 }
+
 
